363e0853a756c44571e2dc031733505891fc1e03
Evan Smith <esincork@gmail.com>
2015-09-23 00:33:15 +0100

    Add Societies and Settings

---
+++ database/migrations/2015_09_22_225808_create_societies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocietiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('societies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('facebook_ref');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('societies');
+    }
+}
