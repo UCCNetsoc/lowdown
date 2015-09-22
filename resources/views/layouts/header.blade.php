aa7bd74e7906959f3fe1951f056bb4c518aa5710
Colm Cahalane <colm@teamwork.com>
2015-09-23 00:10:59 +0100

    while we're on css, add material icons

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,6 +15,7 @@
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
 	<link rel="stylesheet" type="text/css" href="{{ URL::to('/') }}/css/app.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	@yield('extra-css')
 	
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
