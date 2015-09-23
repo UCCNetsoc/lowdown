389d809ce2da7341893a8a889fc328502038d29d
Evan Smith <esincork@gmail.com>
2015-09-23 18:45:40 +0100

    Job requires proper User model

--- app/Jobs/EstablishUserSubscription.php
+++ app/Jobs/EstablishUserSubscription.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
+use App\User;
 use App\Society;
 use App\Subscription;
 
