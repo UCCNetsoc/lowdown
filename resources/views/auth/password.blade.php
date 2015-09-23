7d867dca29d78bf7032e11d0a3ceb1d8f2e99539
Evan Smith <esincork@gmail.com>
2015-09-23 12:40:12 +0100

    Convert auth templates to materialize

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<div class="col m8 offset-m2 s12">
 			<div class="panel panel-default">
 				<div class="panel-heading">Reset Password</div>
 				<div class="panel-body">
@@ -28,14 +28,14 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
+							<label class="col m4 control-label">E-Mail Address</label>
+							<div class="col m6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
+							<div class="col m6 col offset-m4 s12">
 								<button type="submit" class="btn btn-primary">
 									Send Password Reset Link
 								</button>
