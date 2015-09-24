17fc087f41f64da60047e429c7a47b74be2295d3
Colm Cahalane <colm@teamwork.com>
2015-09-24 14:35:53 +0100

    Return Day views.

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -12,6 +12,10 @@ use App\Subscription;
 use App\Society;
 use App\Setting;
 
+use Crypt;
+
+use App\Jobs\UpdateEvents;
+
 class EventsController extends Controller
 {
 	
@@ -29,5 +33,84 @@ class EventsController extends Controller
 	 */
 	public function index( ){
 		return View::make( 'events.home' );
+	}
+
+	public function dayView( $day ){
+		if( Auth::check() ){
+			// The Crypt call here is a bit cheat-y but fuck it, I want this view
+			// to be available to us on the backend AND when we mail out emails to
+			// users so they don't have to sign in.
+			return $this->dayViewForUser($day, Crypt::encrypt(Auth::user()->id) );
+		}
+
+		$values = $this->eventsForDay($day);
+		if(! is_array($values)){
+			return $values;
+		}
+
+		$events = $values['events']->get();
+
+		return view('events.day', ['day' => $values['day'],
+								  'events' => $events]);
+	}
+
+	public function dayJSON( $day ){
+		return $this->eventsForDay($day)['events']->get();
+	}
+
+	public function dayViewForUser( $day, $id ){
+		$id = Crypt::decrypt($id);
+
+		$values = $this->eventsForDay($day);
+
+		if(! is_array($values)){
+			return $values;
+		}
+
+        $soc_ids = DB::table('subscriptions')
+        			 ->where('user_id', $id)
+        			 ->lists('society_id');
+
+        $events = $values['events']->whereIn('society_id', $soc_ids)->get();
+
+		return view('events.day', ['day' => $values['day'],
+								     'events' => $events ]);
+
+	}
+
+	public function eventsForDay($day){
+		$day = strtolower($day);
+		$time = time();
+
+		switch ($day) {
+			case 'monday':
+			case 'tuesday':
+			case 'wednesday':
+			case 'thursday':
+			case 'friday':
+			case 'saturday':
+			case 'sunday':
+			case 'today';
+				$time = strtotime("{$day}");
+				break;
+			
+			default:
+				if(  ( $time = strtotime($day) ) === false ){
+					return Redirect::to('home')->with('message', 'Oops, this page not found!');
+				}
+				break;
+		}
+
+		$day = date('l j F Y', $time);
+
+		$beginOfDay = strtotime("midnight", $time);
+		$endOfDay   = strtotime("tomorrow", $beginOfDay) - 1;
+
+		$events = Event::where('time', '>', date('Y-m-d H:i:s', $beginOfDay))
+					   ->where('time', '<', date('Y-m-d H:i:s', $endOfDay)  )
+					   ->orderBy('time');
+
+		return ['day' => $day, 'events' => $events];
+
 	}
 }
---
+++ resources/views/events/day.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.default')
+
+
+@section('content')
+<main class="container events">
+	<h1>{{$day}}</h1>
+
+	<div class="row">
+		@foreach($events as $event)
+			<div class="col s12 m6 l4">
+				<div class="card">
+					@if( $event->image )
+						<div class="card-image">
+							<a href="https://www.facebook.com/events/{{$event->facebook_id}}">
+								<img src="{{$event->image}}">
+							</a>
+						</div>
+					@endif
+					<div class="card-content">
+						<h4>{{$event->title}}</h5>
+						<h5>{{$event->society()->first()->name}} Society</h5>
+						<p>{{date('H:i, l j F', strtotime($event->time))}}@if($event->location), <strong>{{$event->location}}</strong>@endif</p>
+
+						@if($event->description)
+							<p>{{$event->description}}</p>
+						@endif
+					</div>
+					<div class="card-action">
+					  <a href="https://www.facebook.com/events/{{$event->facebook_id}}">
+					  	Facebook Event
+					  </a>
+					</div>
+				</div>
+			</div>
+		@endforeach
+	</div>
+</main>
+@endsection
