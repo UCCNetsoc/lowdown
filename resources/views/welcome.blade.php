f3eb6745dfb5aaccc9334e75b1d7ff7a842cbacd
Evan Smith <esincork@gmail.com>
2015-09-24 23:21:52 +0100

    Front page styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.default')
 
 @section('content')
-    <main class="row container">
-        <div class="s12 center-align">
-            <h1>Lowdown.</h1>
-            <h2>It's what's goin' on.</h2>
-         </div>
+    <main class="valign-wrapper row welcome-page" style="height: 500px;">
+    	<div class="col s12 valign">
+    		<hr/>
+	        <div class="s12 center-align">
+	            <h1>Lowdown.</h1>
+	            <h2>It's what's goin' on.</h2>
+	        </div>
+	        <hr/>
+	    </div>
     </main>
 @stop
