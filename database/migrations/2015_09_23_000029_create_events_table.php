c8e0557850a7b2519e9db9c37815a99e182b1c6a
Evan Smith <esincork@gmail.com>
2015-09-27 02:56:41 +0100

    Fixed column type
    In practise, facebook uses IDs so large, we have to represent them as
    strings.

--- database/migrations/2015_09_23_000029_create_events_table.php
+++ database/migrations/2015_09_23_000029_create_events_table.php
@@ -15,7 +15,7 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('society_id');
-            $table->unsignedInteger('facebook_id');
+            $table->string('facebook_id');
             $table->string('title');
             $table->text('description');
             $table->string('location');
