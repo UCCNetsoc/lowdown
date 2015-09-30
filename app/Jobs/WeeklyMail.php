137f96665ae81f0fab5577bdfb046b8e85e7623c
Colm Cahalane <colm@teamwork.com>
2015-09-30 01:10:53 +0100

    Check Facebook events before we start mailing

--- app/Jobs/WeeklyMail.php
+++ app/Jobs/WeeklyMail.php
@@ -3,11 +3,19 @@
 namespace App\Jobs;
 
 use App\Jobs\Job;
+use App\Jobs\SendEmail;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\User;
+use App\Event;
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookRedirectLoginHelper;
 
 class WeeklyMail extends Job implements SelfHandling, ShouldQueue
 {
@@ -30,6 +38,41 @@ class WeeklyMail extends Job implements SelfHandling, ShouldQueue
      */
     public function handle()
     {
+        // Same time next week! :D
+        $job = (new \App\Jobs\WeeklyMail())->delay(604800);
+        $this->dispatch($job);
+
+        // We'll just want to double-check our Facebook events still exist
+        // before we email people about them...
+        FacebookSession::setDefaultApplication( getenv('FB_ID'), getenv('FB_SECRET') );
+        $session = FacebookSession::newAppSession();
+
+        try {
+          $session->validate();
+        } catch (FacebookRequestException $ex) {
+          // Session not valid, Graph API returned an exception with the reason.
+          dd($ex);
+        } catch (\Exception $ex) {
+          // Graph API returned info, but it may mismatch the current app or have expired.
+          dd($ex);
+        }
+
+        $all_events = Event::where( 'time', '>', date('Y-m-d H:i:s') )
+                           ->where( 'time', '<', date('Y-m-d H:i:s', time()+604800) )
+                           ->get();
+
+        for($all_events as $event){
+            $request = new FacebookRequest($session, 'GET', "/events/" . $event->facebook_id);
+
+            try{
+                $response = $request->execute();
+            } catch(\Exception $ex) {
+                // Facebook Exception looking up event; probably deleted, should mirror here.
+                $event->delete();
+            }
+        }
+
+
         for(User::where('unsubscribed_email', 'no') as $user){
             $this->dispatch( new SendEmail($user) );
         }
