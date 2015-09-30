59614c2da18dd925d7e088c21c6118a965454d3a
Colm Cahalane <colm@teamwork.com>
2015-09-30 13:44:25 +0100

    Open Graph Imaage

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,11 +4,14 @@
 	<title>@yield('title', env('SITE_TITLE'))</title>
 	<meta charset="utf-8">
 	<meta name="description" content="@yield('description')" />
+	<meta name="author" content="NETSOC" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="shortcut icon" href="{{ URL::to('/') }}/images/favicon.png">
 
+    <meta property="og:title" content="@yield('title', env('SITE_TITLE'))" />
+    <meta property="og:image" content="{{ URL::to('/images/ogimage.png') }}" />
 	
 	<link rel="stylesheet" type="text/css" href="{{ URL::to('/') }}/css/font-awesome.min.css">
 	<link rel="stylesheet" href="{{ URL::to('/') }}/css/normalize.css">
