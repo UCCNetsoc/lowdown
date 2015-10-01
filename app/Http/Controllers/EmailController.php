713d19281cfc90373b7badcdfeccc7f6de1b98eb
Evan Smith <esincork@gmail.com>
2015-10-01 23:47:11 +0100

    Commented Email Controller

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -17,20 +17,35 @@ use App\Jobs\SendEmail;
 class EmailController extends Controller
 {
 
+	/**
+	 * Allows a user unsubscribe from emails
+	 * but keep their subscriptions
+	 * @param  integer $user_id
+	 * @return REDIRECT subscriptions
+	 */
 	public function unsubscribe( $user_id ){
 		$id = Crypt::decrypt( $user_id );
 
 		$user = User::find( $id );
+		// Email unsubs are handled by a flag in the 
+		// User model
 		$user->unsubscribed_email = "yes";
 		$user->save();
 
 		return Redirect::route('subscriptions');
 	}
-	
+
+	/**
+	 * Re-enables weekly emails
+	 * @param  integer $user_id
+	 * @return REDIRECT subscriptions
+	 */
 	public function resubscribe( $user_id ){
 		$id = Crypt::decrypt( $user_id );
 
 		$user = User::find( $id );
+		// Email unsubs are handled by a flag in the 
+		// User model
 		$user->unsubscribed_email = "no";
 		$user->save();
 
