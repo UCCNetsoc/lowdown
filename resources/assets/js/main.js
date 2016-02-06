3e11a7f0ef0dbebba3b48ea1ecca3efebe6a03af
Evan Smith <esincork@gmail.com>
2016-02-06 19:34:09 +0000

    Fix smoothscrolling interference with links

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -7,31 +7,30 @@ $( document ).ready(function($){
 	$('.parallax').parallax();
 
 	// Smooth Scrolling
-    $(document.body).on('click', 'a[href*=#]:not([href=#])', function (e) {
-        e.preventDefault();
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+	$(document.body).on('click', 'a[href^=#]', function (e) {
+		e.preventDefault();
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                var offset = 0;
-                if (target[0].id.indexOf("feature-") == 0) {
-                    offset = 100;
-                }
-                $('html,body').animate({
-                    scrollTop: target.offset().top - offset
-                }, 1000);
-                return false;
-            }
-        }
-    });
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				var offset = 0;
+				if (target[0].id.indexOf("feature-") == 0) {
+					offset = 100;
+				}
+				$('html,body').animate({
+					scrollTop: target.offset().top - offset
+				}, 1000);
+				return false;
+			}
+		}
+	});
 
-    // Webcal Link Rewrite
-    if( $(".calendar-button").length ){
+	// Webcal Link Rewrite
+	if( $(".calendar-button").length ){
 
-    	$(".calendar-button").each(function(){
-	    	var link = $(this).attr('href');
-
+		$(".calendar-button").each(function(){
+			var link = $(this).attr('href');
 			var link = link.replace(window.location.protocol + "//", "webcal://");
 			
 			if(navigator.platform.toUpperCase().indexOf('LINUX')>=0 || navigator.platform.toUpperCase().indexOf('WIN')>=0){
@@ -39,8 +38,8 @@ $( document ).ready(function($){
 			}
 
 			$(this).attr('href', link);
-    	});
-    }
+		});
+	}
 });
 
 /*
@@ -54,7 +53,7 @@ function updateList(){
 		societies.push( $(this).attr('id') );
 	});
 
- 	var json = JSON.stringify(societies);
+	var json = JSON.stringify(societies);
 
 	$('input[name=allSubscriptions]').val( json );
 }
